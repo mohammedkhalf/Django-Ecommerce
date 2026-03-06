b093cb77a14c2aef33fa343dd45e04be9bc4cb6d
Mohamed Khalf <mohamed.khalf@tamara.co>
2026-03-06 06:04:13 +0200

    Nested Serializers, SerializerMethodField and Serializer Relations

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('products/', views.product_list),
-    path('products/<int:pk>/', views.product_details)
+    path('products/<int:pk>/', views.product_details),
+    path('orders/', views.order_list)
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
-from api.serializers import ProductSerializer
-from api.models import Product
+from api.serializers import ProductSerializer, OrderSerializer
+from api.models import Product, Order
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -15,5 +15,12 @@ def product_list(request):
 def product_details(request, pk):
     product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
     
