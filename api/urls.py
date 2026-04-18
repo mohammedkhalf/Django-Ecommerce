a6b705cb3f9f927d1e6a2e350e53069ad0756187
Mohamed Khalf <mohamed.khalf@tamara.co>
2026-04-18 05:37:29 +0200

    write Test Cases for APIs

--- api/urls.py
+++ api/urls.py
@@ -8,6 +8,6 @@ router.register('orders', views.OrderViewSet)
 urlpatterns = [
     path('products/', views.ProductListCreateAPIView.as_view()),
     path('products/info', views.ProductInfoAPIView.as_view()),
-    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view()),
+    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
     path('users/', views.UserListView.as_view()),
 ] + router.urls
